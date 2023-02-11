34cee08feda4459534f67ed2964e2f22452cc73b
Shailesh <gajbhiye719@gmail.com>
2023-02-11 09:54:20 +0530

    Solution

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -3,10 +3,9 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("books")
@@ -16,8 +15,9 @@ public class BookController {
 
     // One example controller, make the rest by yourself
     @PostMapping("/create-book")
-    public ResponseEntity createBook(@RequestBody Book book){
+    public ResponseEntity createBook(@RequestBody Book book ){
         Book newbook = bookService.createBook(book);
         return new ResponseEntity<>(newbook, HttpStatus.CREATED);
     }
+
 }
--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -6,36 +6,36 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BookRepository {
 
-    public BookRepository(){
-        
+
+
+    public BookRepository() {
     }
 
-    public Book save(Book book){
+    public Book save(Book book) {
+       return null ;
+    }
+
+    public Book findBookById(int id) {
         return null;
     }
 
-    public Book findBookById(int id){
+    public List<Book> findAll() {
+        return null ;
+    }
 
+    public void deleteBookById(int id) {
+        return ;
+    }
+
+    public void deleteAll() {
+       return ;
+    }
+
+    public List<Book> findBooksByAuthor(String author) {
         return null;
     }
 
-    public List<Book> findAll(){
-        return null;
-    }
-
-    public void deleteBookById(int id){
-        return;
-    }
-
-    public void deleteAll(){
-        return;
-    }
-
-    public List<Book> findBooksByAuthor(String author){
-        return null;
-    }
-
-    public List<Book> findBooksByGenre(String genre){
-        return null;
+    public List<Book> findBooksByGenre(String genre) {
+        return null ;
     }
 }
